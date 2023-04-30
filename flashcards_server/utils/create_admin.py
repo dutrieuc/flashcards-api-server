36e57c84a781e1d8a055701eb40841b72b9c3058
Cyril Dutrieux (cydu) <cydu@odoo.com>
2023-04-30 22:05:33 +0200

    Adding create_admin script

---
+++ flashcards_server/utils/create_admin.py
@@ -0,0 +1,30 @@
+import contextlib
+import asyncio
+
+from flashcards_server.database import get_async_session, get_user_db
+from flashcards_server.schemas import UserCreate
+from flashcards_server.users import get_user_manager
+from fastapi_users.exceptions import UserAlreadyExists
+
+get_async_session_context = contextlib.asynccontextmanager(get_async_session)
+get_user_db_context = contextlib.asynccontextmanager(get_user_db)
+get_user_manager_context = contextlib.asynccontextmanager(get_user_manager)
+
+
+async def create_user(email: str, password: str, is_superuser: bool = False):
+    try:
+        async with get_async_session_context() as session:
+            async with get_user_db_context(session) as user_db:
+                async with get_user_manager_context(user_db) as user_manager:
+                    user = await user_manager.create(
+                        UserCreate(
+                            email=email, password=password, is_superuser=is_superuser
+                        )
+                    )
+                    print(f"User created {user}")
+    except UserAlreadyExists:
+        print(f"User {email} already exists")
+
+
+def create_admin():
+    asyncio.run(create_user("admin", "admin", is_superuser=True))
